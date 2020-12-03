be25bf9451aede0426c44811e8f868f2a8914da5
Elena Antonenkova <elena.antonenkova@gmail.com>
2020-12-03 13:45:19 +0300

    * cleanup

--- common/tests/unit/models/MessageTest.php
+++ common/tests/unit/models/MessageTest.php
@@ -4,32 +4,12 @@ namespace common\tests\unit\models;
 
 use Yii;
 use common\models\Message;
-use common\fixtures\UserFixture;
 
 /**
  * Login form test
  */
 class MessageTest extends \Codeception\Test\Unit
 {
-    /**
-     * @var \common\tests\UnitTester
-     */
-    protected $tester;
-
-
-    /**
-     * @return array
-     */
-    public function _fixtures()
-    {
-        return [
-            'user' => [
-                'class' => UserFixture::className(),
-                'dataFile' => codecept_data_dir() . 'user.php'
-            ]
-        ];
-    }
-
     public function testRevertCharacters()
     {
         $message = new Message([
